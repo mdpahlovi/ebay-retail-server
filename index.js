4c3433aa679faad01e5bbe7ad2e8131ea6a1d655
MD Pahlovi <mdpahlovi07@gmail.com>
2022-11-23 22:11:36 +0600

    Email Save To DB & JWT Generate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
@@ -13,14 +14,29 @@ const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const database = async () => {
-    const test = client.db("text").collection("test");
-    await test.insertOne({ name: "Pahlovi" });
+    const usersCollection = client.db("ebay").collection("users");
+
+    // Save User To DB , Generate & Sent JWT Token to site
+    app.put("/user/:email", async (req, res) => {
+        const { email } = req.params;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: user,
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc, options);
+        const token = jwt.sign(user, process.env.ACC_Token, {
+            expiresIn: "1d",
+        });
+        res.send({ result, token });
+    });
 };
 
 database().catch((err) => console.log(`${err.name} ${err.message}`));
 
 app.get("/", (req, res) => {
-    res.send("assignment-12 Server Running");
+    res.send("Ebay Server Running");
 });
 
 app.listen(port, () => {
