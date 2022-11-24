caeb1765cc71dff1d31f7918d81e03481b2fbd7b
MD Pahlovi <mdpahlovi07@gmail.com>
2022-11-24 16:43:12 +0600

    JWT & Admin Verify, Get All User & User By Email Done

--- index.js
+++ index.js
@@ -13,9 +13,36 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster.qheqolp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACC_Token, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 const database = async () => {
-    const usersCollection = client.db("ebay").collection("users");
-    const blogsCollection = client.db("ebay").collection("blogs");
+    const userCollection = client.db("ebay").collection("users");
+    const blogCollection = client.db("ebay").collection("blogs");
+    const productCollection = client.db("ebay").collection("products");
+
+    // Verify Admin
+    const verifyAdmin = async (req, res, next) => {
+        const decodedEmail = req.decoded.email;
+        const query = { email: decodedEmail };
+        const user = await userCollection.findOne(query);
+        if (user?.role !== "admin") {
+            return res.status(403).send({ message: "Forbidden Access" });
+        }
+        next();
+    };
 
     // Save User To DB , Generate & Sent JWT Token to site
     app.put("/user/:email", async (req, res) => {
@@ -26,18 +53,46 @@ const database = async () => {
         const updateDoc = {
             $set: user,
         };
-        const result = await usersCollection.updateOne(filter, updateDoc, options);
+        const result = await userCollection.updateOne(filter, updateDoc, options);
         const token = jwt.sign(user, process.env.ACC_Token, {
             expiresIn: "1d",
         });
         res.send({ result, token });
     });
 
+    // Get All User For Admin
+    app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+        const cursor = userCollection.find({});
+        const users = await cursor.toArray();
+        res.send(users);
+    });
+
+    // Get The User Bu Email
+    app.get("/user/:email", verifyJWT, async (req, res) => {
+        const { email } = req.params;
+        const decodedEmail = req.decoded.email;
+
+        if (email !== decodedEmail) {
+            return res.status(403).send({ message: "Forbidden Access" });
+        }
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        res.send(user);
+    });
+
     // Get Blogs
     app.get("/blogs", async (req, res) => {
-        const curser = blogsCollection.find({});
+        const curser = blogCollection.find({});
         const blogs = await curser.toArray();
         res.send(blogs);
+    });
+
+    app.get("/categories", async (req, res) => {
+        const curser = productCollection.find({});
+        const products = await curser.toArray();
+        const allCategories = products.map((product) => product.category);
+        const categories = allCategories.filter((v, i, a) => a.indexOf(v) === i);
+        res.send(categories);
     });
 };
 
